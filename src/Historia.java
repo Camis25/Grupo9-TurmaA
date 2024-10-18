dfe1b6aff8dfe2887f3ab3858e90cd62f6e22c07
Jaine09 <jainecosta0911@gmail.com>
2024-10-18 20:21:17 -0300

    ajustar escolha do companheiro

--- src/Historia.java
+++ src/Historia.java
@@ -18,8 +18,32 @@ public class Historia {
         System.out.print("Qual o nome do seu personagem: ");
         nomePersonagem = entrada.nextLine();
         System.out.println("Escolha seu companheiro de aventura: ");
-        System.out.print("1)Orion - Habilidoso em lógica. (pode receber dicas em 1 desafio, E diminui o tempo de resposta em 10seg)\n2)Kira - Criativa e imprevisível. (ao ser chamada pode fazer o jogador pula a questão OU dificultar a pergunta)\n3)Dante - Sempre tem uma carta na manga. (Muda o desafio podendo retornar um mais fácil OU complicado)");
+        System.out.print("1)Orion - Habilidoso em lógica. (pode receber dicas em 1 desafio, E diminui o tempo de resposta em 10seg)\n2)Kira - Criativa e imprevisível. (ao ser chamada pode fazer o jogador pula a questão OU dificultar a pergunta)\n3)Dante - Sempre tem uma carta na manga. (Muda o desafio podendo retornar um mais fácil OU complicado)\n");
         companheiro = entrada.nextInt();
+
+        do {
+            
+            switch (companheiro) {
+                case 1:
+                    
+                    break;
+                case 2:
+    
+                    break;
+                case 3:
+    
+                    break;
+            
+                default:
+                    System.out.print("Companheiro não identificado!\n");
+                    break;
+            }
+            System.out.print("Escolha seu companheiro de aventura: ");
+            companheiro = entrada.nextInt();
+            
+        } while (companheiro > 3);
+
+        
 
         entrada.close();
     }
