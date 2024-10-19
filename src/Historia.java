10f44db07bceb1db800f85a4cbc050715fb27484
Jaine09 <jainejesus0911@gmail.com>
2024-10-19 20:19:31 -0300

    junção do menu na historia

--- src/Historia.java
+++ src/Historia.java
@@ -4,10 +4,40 @@ import src.config.TextoAnimado;
 
 public class Historia {
     public static void main(String[] args) {
-       Historia historia = new Historia();
-       historia.HistoriaInicial(null, 0);
-        
+        Scanner entrada = new Scanner(System.in);
+        String apresentacaoJogo = "Bem vindo ao Jogo dos Destinos, um jogo cheio de aventura e desafios. Escolha suas ações e determine o seu futuro.\nO que deseja fazer agora?";
+        TextoAnimado.aparecerTexto(apresentacaoJogo, 90);
+
+        System.out.println(" 1) Jogar\n 2) Regras\n 3) Créditos\n 4) Sair ");
+        int opcao = entrada.nextInt();
+
+        switch (opcao) {
+            case 1:
+                Historia historia = new Historia();
+                historia.HistoriaInicial(apresentacaoJogo, opcao);
+                break;
+            case 2:
+                System.out.println("Responda todas as questões dento de 5 minutos");
+                System.out.println("Utilize as habilidades de seu companheiro com sabedoria, pois só pode ser utilizada uma vez");
+                System.out.println("Existe somente uma alternativa correta");
+                break;
+            case 3:
+                System.out.println("Turma A - Grupo 9");
+                System.out.println("Feito por:");
+                System.out.println("Jaíne");
+                System.out.println("Camile");
+                System.out.println("Leonardo");
+                System.out.println("Pedro");
+                break;
+            case 4:
+                System.out.println("Adeus até a próxima aventura");
+            default:
+                break;
+        }
+
+        entrada.close();
     }
+        
 
     public void HistoriaInicial(String nomePersonagem, int companheiro){
         Scanner entrada = new Scanner(System.in);
@@ -27,7 +57,7 @@ public class Historia {
         System.out.print("""
             1)Orion - Habilidoso em lógica. (pode receber dicas em 1 desafio, E diminui o tempo de resposta em 10seg)
             \n2)Kira - Criativa e imprevisível. (ao ser chamada pode fazer o jogador pula a questão OU dificultar a pergunta)
-            \n3)Dante - Sempre tem uma carta na manga. (Muda o desafio podendo retornar um mais fácil OU complicado)\n");
+            \n3)Dante - Sempre tem uma carta na manga. (Muda o desafio podendo retornar um mais fácil OU complicado)\n
             """);
                    
         companheiro = entrada.nextInt();
@@ -68,7 +98,7 @@ public class Historia {
             companheiroEscolhido = "Dante";
             System.out.println("Ótima escolha! Se prepare para embarcar nessa aventura com " + companheiroEscolhido);
         }
-        
+
         entrada.close();
     }
 }
