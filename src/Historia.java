9f3fbcc70ab9abe5d8ab4bac3f1dfaa77cbc6411
Camis25 <camisvitorisarosasantos2005@gmail.com>
2024-10-19 21:18:26 -0300

    Junção de classes

--- src/Historia.java
+++ src/Historia.java
@@ -1,9 +1,43 @@
 package src;
 import java.util.Scanner;
-import src.config.TextoAnimado;
+
 
 public class Historia {
+    public class TextoAnimado {
 
+    
+        public static void aparecerTexto(String texto, int delay) {
+            for (char letra : texto.toCharArray()) {
+                System.out.print(letra);
+                try {
+                    Thread.sleep(delay); 
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt(); 
+                }
+            }
+            System.out.println(); 
+        }
+    }
+    
+
+    public class Personagens {
+        public String criarPersonagem(String personagem) {
+            String texto = "Ao escolher o personagem, uma luz intensa surge de dentro do console, te sugando para dentro de um mundo "
+                         + "\nmágico. No começo, você fica confusa, mas logo se dá conta de que está no corpo do personagem escolhido. O "
+                         + "\nambiente é uma mistura de paisagens bizarras e criaturas excêntricas. Ao perceber que está dentro do jogo, "
+                         + "\nvocê se desespera, pois não sabe como sair de lá. Observando o ambiente, se depara com uma longa estrada de "
+                         + "\ntijolos amarelos e decide segui-la. Quanto mais você avança, a estrada adentra uma floresta escura e sombria. "
+                         + "\nAo fim da estrada, encontra uma porta que parece conter algum tipo de enigma para abri-la. De repente, uma "
+                         + "\nvoz ecoa de dentro da floresta: \"" + personagem + "! Estávamos empolgados em ver você por aqui. Que bom que você "
+                         + "\ndecidiu seguir o seu destino... ou não. Mas hey, pelo menos agora temos alguém para culpar quando as coisas "
+                         + "\nderem errado!\" A floresta estava falando com você: \"Mas enfim, com certeza você deve estar se perguntando como "
+                         + "\nsair desse lugar. Durante a sua trajetória, terão 5 desafios de lógica que te levarão para fora do jogo, claro, "
+                         + "\nisso se você conseguir continuar o jogo. A primeira tarefa era abrir a porta trancada, marcada por símbolos "
+                         + "\nestranhos. Ao descobrir um papel com um código, ela rapidamente percebeu que precisava...";
+            return texto;
+        }
+    }
+    
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         String apresentacaoJogo = "Bem vindo ao Jogo dos Destinos, um jogo cheio de aventura e desafios.\nEscolha suas ações e determine o seu futuro.\nO que deseja fazer agora?";
@@ -54,8 +88,6 @@ public class Historia {
 
         System.out.print("Qual o nome do seu personagem: ");
         nomePersonagem = entrada.nextLine();
-        
-
 
         do {
             System.out.println("Escolha seu companheiro de aventura: ");
